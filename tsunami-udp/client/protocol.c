c9e0344a8a05442c99db085ffacc8df0d4bf4e8f
jwagnerhki <>
2006-10-25 14:53:16 +0000

    removed superfluous ACK on GET*

--- tsunami-udp/client/protocol.c
+++ tsunami-udp/client/protocol.c
@@ -204,8 +204,6 @@ int ttp_open_transfer(ttp_session_t *session, const char *remote_filename, const
        fprintf(stderr, "enter filename from above list> ");
        fgets(newfile, MAX_COMMAND_LENGTH, stdin);
        strcat(newfile, "\n");
-       
-       if (fwrite(newfile, strlen(newfile), 1, session->server) < 1) return warn("Could not submit back ACK on received server files list");
     } /* end of multimode session*/ 
         
     
@@ -586,6 +584,9 @@ int ttp_update_stats(ttp_session_t *session)
 
 /*========================================================================
  * $Log$
+ * Revision 1.6  2006/10/25 14:20:31  jwagnerhki
+ * attempt to support multimode get already implemented in Jamil server
+ *
  * Revision 1.5  2006/10/19 08:06:31  jwagnerhki
  * fix STATS_MATLABFORMAT ifndef to ifdef
  *
