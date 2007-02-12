381b18e16294a00ff88ccee0f948374d833507da
jwagnerhki <>
2007-02-12 13:41:21 +0000

    mktime() post-fix now parsing really to utc, added 'dl' same as 'flen', added some EVN formats, added parse validity flag

--- tsunami-udp/rtserver/parse_evn_filename.h
+++ tsunami-udp/rtserver/parse_evn_filename.h
@@ -1,15 +1,19 @@
 #ifndef PARSE_EVN_FILENAME
 #define PARSE_EVN_FILENAME
 
+#include <time.h>
+#include <sys/time.h>
+
 struct evn_filename {
-	char *exp_name;
-	char *station_code;
-	char *scan_name;
-	char *data_start_time_ascii;
-	double data_start_time;
-	char **auxinfo; /* pointer to (array of pointers to aux info elements) */
-	int nr_auxinfo;
-	char *file_type;
+	char    *exp_name;
+	char    *station_code;
+	char    *scan_name;
+	char    *data_start_time_ascii;
+	double   data_start_time;         /* system tick seconds time_t (integer) or with fractional seconds (double) */
+	char   **auxinfo;                 /* pointer to (array of pointers to aux info elements) */
+	int      nr_auxinfo;
+	char    *file_type;
+    char     valid;
 };
 
 struct evn_filename *parse_evn_filename(char *filename);
@@ -20,6 +24,9 @@ char *get_aux_entry(char *key, char **auxinfo, int nr_auxinfo);
 
 /*
  * $Log$
+ * Revision 1.2  2006/10/25 12:14:07  jwagnerhki
+ * added cvs log line
+ *
  */ 
 
 
